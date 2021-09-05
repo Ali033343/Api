71e5dbf4275f553c9131075f4f98cf38806c298f
aliahmed12nov <aliahmed12nov@gmail.com>
2021-09-06 00:15:51 +0500

    1st commit

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _Context;
+
+        public UsersController(DataContext Context)
+        {
+            _Context = Context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUsers>>> GetUsers()
+        {
+            var users =await  _Context.Users.ToListAsync();
+            return  users;
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUsers>> GetUsers(int id)
+        {
+            var user = await _Context.Users.FindAsync(id);
+            return user;
+
+        }
+    }
+}
---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUsers> Users { get; set; }
+    }
+}
